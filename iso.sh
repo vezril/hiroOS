daed406e375513b0123406f6bdcd92cc709e4112
Calvin Ference <calvin.ference@linux.com>
2016-03-30 13:26:14 -0400

    Changed myos mentions to hiroos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/hiroos.kernel isodir/boot/hiroos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "hiroos" {
+	multiboot /boot/hiroos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o hiroos.iso isodir

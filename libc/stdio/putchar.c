0bca601b5c57cdbe1cc9679a5ebee0f67614596d
Calvin Ference <calvin.ference@linux.com>
2016-03-31 10:27:15 -0400

    Refactored the rest of libc to match linux kernel coding style.

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -4,8 +4,7 @@
 #include <kernel/tty.h>
 #endif
 
-int putchar(int ic)
-{
+int putchar(int ic) {
 #if defined(__is_hiroos_kernel)
 	char c = (char) ic;
 	terminal_write(&c, sizeof(c));
--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 __attribute__((__noreturn__))
-void abort(void)
-{
+void abort(void) {
 	// TODO: Add proper kernel panic.
 	printf("Kernel Panic: abort()\n");
-	while ( 1 ) { }
+	while(1) { }
 	__builtin_unreachable();
 }

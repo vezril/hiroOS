812794ca919802fdbef704403ab60236c7f369c7
Calvin Ference <calvin.ference@linux.com>
2016-04-05 15:39:58 -0400

    Addaded itoa implementation and atoi skeleton

---
+++ libc/stdlib/atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int atoi(const char * str) {
+    // int result=0;
+    // int num;
+    // bool negative = false;
+    // size_t expnt = strlen(str);
+    // size_t i, j;
+    //
+    // if(str[0] == '-'){
+    //     negative = true;
+    // }
+    //
+    // // Probably a better way to do this
+    // for(i=expnt;i>0;i--){
+    //     num = (int) str[i]-48;
+    //     for(j=0;j<expnt;j++){
+    //         num *= 10;
+    //     }
+    //     result += num;
+    // }
+    //
+    // if(negative)
+    //     result *= -1;
+    // return result;
+}
